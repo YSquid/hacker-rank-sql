3ae92b0883e3e35a81783dd41be0983d9ec6520e
Ysquid <ahk648@gmail.com>
2023-03-14 14:28:29 -0600

    weather_19 solution

---
+++ weather_19.sql
@@ -0,0 +1,14 @@
+--Consider P1(a,c) and P2(b,d)  and  to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+--Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+SELECT
+
+MIN(LAT_N) as a,
+MAX(LAT_N) as b,
+MIN(LONG_W) as c,
+MAX(LONG_W) as d,
+ROUND(SQRT (POWER((MAX(LAT_N) - MIN(LAT_N)), 2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)),4) as Distance
+FROM station
+
+
